a615b2718298f0889fe4fc87d69b7cb332fea0df
MuzzammilShah <muhammed.muzzammilshah@gmail.com>
2025-04-10 06:40:24 +0530

    Finished 2.2 containers

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,55 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+# def portfolio_list(filename):
+
+#     portfolio = []
+
+#     with open(filename, 'rt') as f:
+
+#         rows = csv.reader(f)
+#         headers = next(rows)
+#         for row in rows:
+#             portfolio.append((row[0], int(row[1]), float(row[2])))
+
+#     return portfolio
+
+# def main():
+#     listp = portfolio_list('Data/portfolio.csv')
+#     for item in listp:
+#         print(item)
+
+# if __name__ == '__main__':
+#     main ()
+
+def portfolio_dict(filename):
+
+    portfolio = {}
+
+    with open(filename, 'rt') as f:
+
+        rows = csv.reader(f)
+        headers = next(rows)
+        # for i, row in enumerate(rows):
+        #     portfolio[i] = {
+        #         'name': row[0],
+        #         'shares': row[1],
+        #         'price': row[2]
+        #     }
+        for row in rows:
+            portfolio[row[0]] = {
+                'shares': int(row[1]),
+                'price': float(row[2])
+            }
+
+    return portfolio
+
+def main():
+    listp = portfolio_dict('Data/portfolio.csv')
+    print(listp)
+
+if __name__ == '__main__':
+    main ()
